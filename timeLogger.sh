c2d42676a7f9af0347b27ba7c9ef540bf25d486e
Charles Shinaver <bshinaver@gmail.com>
2015-03-15 16:09:55 -0400

    Added trees

--- timeLogger.sh
+++ timeLogger.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 rawTimeData=".rawTimeData"
-timeNow=`date +"%m-%d-%Y %r"`
+timeNow=`date +"%m-%d-%Y;%r"`
+timeNowNice=`date +"%m-%d-%Y %r"`
 
 # Check if in correct project directory
 if ! [[ "$(expr "$(pwd)" : '.*/\(.*\)')" = "TypeRight" ]]
@@ -11,6 +12,19 @@ then
 fi
 
 echo "Welcome to TimeLogger!"
+echo "               ,@@@@@@@,
+       ,,,.   ,@@@@@@/@@,  .oo8888o.
+    ,&%%&%&&%,@@@@@/@@@@@@,8888\\88/8o
+   ,%&\\%&&%&&%,@@@\\@@@/@@@88\\88888/88'
+   %&&%&%&/%&&%@@\\@@/ /@@@88888\\88888'
+   %&&%/ %&%%&&@@\\ V /@@' \`88\\8 \`/88'
+   \`&%\\ \` /%&'    |.|        \\ '|8'
+       |o|        | |         | |
+       |.|        | |         | |
+    \\\\/ ._\\//_/__/  ,\\_//__\\\\/.  \\_//__/_
+"
+echo "             ^^^^^^^^^^^^^^^^^^^^^^"
+echo "                     Trees         "
 
 # Get name
 if ! [ -e .name ]; then
@@ -27,10 +41,10 @@ echo "Are you checking in (0) or out (1)?"
 read ans
 
 if [ $ans -eq 0 ]; then
-    echo "$name checking in at $timeNow"
+    echo "$name checking in at $timeNowNice"
     echo "$name;in;$timeNow" >> $rawTimeData
 elif [ $ans -eq 1 ]; then
-    echo "$name checking out at $timeNow"
+    echo "$name checking out at $timeNowNice"
     echo "$name;out;$timeNow" >> $rawTimeData
 else
     echo "Please enter 0 or 1 for checking in or out"
