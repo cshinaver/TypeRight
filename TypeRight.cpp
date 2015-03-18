72be58d6aabc669f5ffb0d6d4e41189c052c00fd
Charles Shinaver <bshinaver@gmail.com>
2015-03-18 13:17:41 -0400

    Fixed intents, and changed back comments >.<

--- TypeRight.cpp
+++ TypeRight.cpp
@@ -2,7 +2,7 @@
  * TypeRight.cpp
  * Charles Shinaver
  * Main game implementation
- */
+*/
 
 #include "TypeRight.h"
 #include "Bruh.h"
@@ -12,104 +12,104 @@ void backgroundDemoFunction(SDLWrapper &sw);
 
 TypeRight::TypeRight()
 {
-	/*
-	 * Default constructor
-	 */
+    /*
+     * Default constructor
+    */
 
-	bgX=0; // background starts at beginning, leftmost x coordinate of texture
-	bgY=0; // doesn't change
-	
-	// Init window and initial surface
-	sw.init();
+    bgX=0; // background starts at beginning, leftmost x coordinate of texture
+    bgY=0; // doesn't change
 
-	//initializes textures
-	backgroundTex = sw.loadTexture("typeRightBackground1Double.png", 0, 0, 0xFF, 0xFF);
+    // Init window and initial surface
+    sw.init();
+
+    //initializes textures
+    backgroundTex = sw.loadTexture("typeRightBackground1Double.png", 0, 0, 0xFF, 0xFF);
 }
 
 void TypeRight::startGame()
 {
-	/*
-	 * Starts game
-	 */
+    /*
+     * Starts game
+    */
 
 
-	// Set quit status
-	quit = false;
+    // Set quit status
+    quit = false;
 
-	//##################
-	//# Main Game Loop #
-	//##################
+    //##################
+    //# Main Game Loop #
+    //##################
 
-	// For demo
-	int count=0;
-	while (!quit)
-	{
-		checkForEvents();
+    // For demo
+    int count=0;
+    while (!quit)
+    {
+        checkForEvents();
 
-		// Clear screen
-		SDL_SetRenderDrawColor(sw.renderer, 0xFF, 0xFF, 0xFF, 0xFF );        
-		sw.clearWindow();
+        // Clear screen
+        SDL_SetRenderDrawColor(sw.renderer, 0xFF, 0xFF, 0xFF, 0xFF );        
+        sw.clearWindow();
 
-		//demoFunction(sw);
-		//backgroundDemoFunction(sw); // tests the new background
-		cout << "count=" << count << endl; // counts while loop iterations
-		count++;
+        //demoFunction(sw);
+        //backgroundDemoFunction(sw); // tests the new background
+        cout << "count=" << count << endl; // counts while loop iterations
+        count++;
 
-		//Scroll Background is somewhere near here???????
-		scrollBackground();
+        //Scroll Background is somewhere near here???????
+        scrollBackground();
 
-		// Update screen
-		sw.updateWindow();
-	}
+        // Update screen
+        sw.updateWindow();
+    }
 
-	// Quit SDL
-	sw.quit();
-	
-	// Free textures
-	backgroundTex.freeTexture();
+    // Quit SDL
+    sw.quit();
+
+    // Free textures
+    backgroundTex.freeTexture();
 }
 
 void TypeRight::scrollBackground()
 {
-	double backFract = .5;
-	// Offset of the background
-	bgX+=4;
-	if (bgX >= backgroundTex.getWidth()*(1-backFract) ) // background1 is 951 x 521 pixels, shouldn't hard code this. FIX LATER!!
-	
-//if (bgX >= backgroundTex.getWidth()*(1-backFract) ) // background1 is 951 x 521 pixels, shouldn't hard code this. FIX LATER!!
-	{
-		bgX=0; // reset to the beginning of the background
-	}	
-	//cout << "bgX=" << bgX << endl;
-	
+    double backFract = .5;
+    // Offset of the background
+    bgX+=4;
+    if (bgX >= backgroundTex.getWidth()*(1-backFract) ) // background1 is 951 x 521 pixels, shouldn't hard code this. FIX LATER!!
 
-// destination rectangle
-	SDL_Rect src = {bgX, bgY, backgroundTex.getWidth()*backFract, backgroundTex.getHeight()};
-	SDL_Rect dest = {0, 0, sw.SCREEN_WIDTH, sw.SCREEN_HEIGHT};
-	sw.renderTextureToWindow(backgroundTex, &src, &dest);
+        //if (bgX >= backgroundTex.getWidth()*(1-backFract) ) // background1 is 951 x 521 pixels, shouldn't hard code this. FIX LATER!!
+    {
+        bgX=0; // reset to the beginning of the background
+    }	
+    //cout << "bgX=" << bgX << endl;
+
+
+    // destination rectangle
+    SDL_Rect src = {bgX, bgY, backgroundTex.getWidth()*backFract, backgroundTex.getHeight()};
+    SDL_Rect dest = {0, 0, sw.SCREEN_WIDTH, sw.SCREEN_HEIGHT};
+    sw.renderTextureToWindow(backgroundTex, &src, &dest);
 }
 
 
 
 void TypeRight::checkForEvents()
 {
-	/*
-	 * Checks for any events in queue and responds if any
-	 * Put responses to events here
-	 */
+    /*
+     * Checks for any events in queue and responds if any
+     * Put responses to events here
+    */
 
-	// Event handler
-	SDL_Event e;
+    // Event handler
+    SDL_Event e;
 
-	// Event handler loop
-	while (SDL_PollEvent( &e ) != 0)
-	{
-		// User quits
-		if (e.type == SDL_QUIT)
-		{
-			quit = true;
-		}
-	}
+    // Event handler loop
+    while (SDL_PollEvent( &e ) != 0)
+    {
+        // User quits
+        if (e.type == SDL_QUIT)
+        {
+            quit = true;
+        }
+    }
 }
 
 void demoFunction(SDLWrapper &sw)
@@ -124,15 +124,15 @@ void demoFunction(SDLWrapper &sw)
 
 void backgroundDemoFunction(SDLWrapper &sw)
 {
-	/*
-	 * Similar to demoFunction above, except with background1 this time
-	 */
+    /*
+     * Similar to demoFunction above, except with background1 this time
+    */
 
-	TRTexture back = sw.loadTexture("typeRightBackground1.png"); // new background
-	sw.renderTextureToWindow(back);
-	TRTexture foo = sw.loadTexture("foo.png", 1, 0, 0xFF, 0xFF);
-	SDL_Rect renderQuad = {sw.SCREEN_WIDTH / 2, sw.SCREEN_HEIGHT / 2, foo.getWidth(), foo.getHeight()};
-	sw.renderTextureToWindow(foo, NULL, &renderQuad);
+    TRTexture back = sw.loadTexture("typeRightBackground1.png"); // new background
+    sw.renderTextureToWindow(back);
+    TRTexture foo = sw.loadTexture("foo.png", 1, 0, 0xFF, 0xFF);
+    SDL_Rect renderQuad = {sw.SCREEN_WIDTH / 2, sw.SCREEN_HEIGHT / 2, foo.getWidth(), foo.getHeight()};
+    sw.renderTextureToWindow(foo, NULL, &renderQuad);
 }
 
 
