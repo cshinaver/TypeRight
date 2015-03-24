7f93b682bfe45dbc2e6ec93162590ded2d1831ce
Charles Shinaver <bshinaver@gmail.com>
2015-03-24 18:32:07 -0400

    Set texture does not need to be public in background

--- Background.h
+++ Background.h
@@ -16,10 +16,10 @@ class Background : public Sprite
     public:
         Background();
         Background(TRTexture tex);
-        void setTexture(TRTexture tex);
         virtual void move();
         void setTexturePath(string path);
     private:
+        void setTexture(TRTexture tex);
         double db; // derivative of background over time, speed coef of each background
 };
 
