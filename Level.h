36e838b83eb774b59270aea4ba7561ece7172672
Charles Shinaver <bshinaver@gmail.com>
2015-03-24 18:35:22 -0400

    Made level class abstract and added startLevel and background properties

--- Level.h
+++ Level.h
@@ -3,3 +3,12 @@
  * Charles Shinaver
  * Level Class
 */
+
+#include "Background.h"
+class Level
+{
+    public:
+        virtual void startLevel() = 0;
+    private:
+        Background levelBackground;
+};
