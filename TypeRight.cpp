471bcd73e9566065708896f5d1c11e72e643c09c
Madelyn <mnelso12@nd.edu>
2015-04-10 15:06:39 -0400

    Added levels

--- TypeRight.cpp
+++ TypeRight.cpp
@@ -46,11 +46,11 @@ void TypeRight::startGame()
 //    Level3 l3(sw); // hardest, Notre Dame
 //    l3.startLevel();
 
-//    Level2 l2(sw); // middle, scary night city
-//    l2.startLevel();
+    Level2 l2(sw); // middle, scary night city
+    l2.startLevel();
 
-    Level1 l1(sw); // easiest, country field background
-    l1.startLevel();
+//    Level1 l1(sw); // easiest, country field background
+//    l1.startLevel();
 
     // Quit SDL
     sw.quit();
