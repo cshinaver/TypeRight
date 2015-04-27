0254f1824d419657aefcccd0d4420e6296de51fc
erunburg <ElliottRunburg@elliotts-mbp.dhcp.nd.edu>
2015-04-27 16:38:47 -0400

    Updated the driver file to show the game over screen.

--- TypeRight.cpp
+++ TypeRight.cpp
@@ -15,6 +15,8 @@
 #include "Level1.h"
 #include "Level2.h"
 #include "Level3.h"
+#include "GameOver.h"
+#include <unistd.h>
 
 TypeRight::TypeRight()
 {
@@ -35,6 +37,7 @@ void TypeRight::startGame()
 
     // Set next level
     int nextLevel = 1;
+    int play = 1;
 
     //##################
     //# Main Game Loop #
@@ -44,23 +47,28 @@ void TypeRight::startGame()
     Level1 l1(sw); // easiest, country field background
     Level2 l2(sw); // middle, scary night city
     Level3 l3(sw); // hardest, Notre Dame
-    
 
     while (nextLevel)
     {
-        switch (nextLevel)
+        switch (nextLevel) // 0 will kill the program, 4 will show a winning screen, 5 will show a losing screen
         {
             case 1:
-                nextLevel = l1.startLevel();
-                break;
+               nextLevel = l1.startLevel();
+               break;
             case 2:
-                nextLevel = l2.startLevel();
-                break;
+               nextLevel = l2.startLevel();
+               break;
             case 3:
-                nextLevel = l3.startLevel();    
-                break;
+               nextLevel = l3.startLevel();    
+               break;
+	    case 4:
+               // This is where a winning scenario would go
+	       break;
+ 	    case 5:
+	       GameOver gg(sw); // Game over screen
+	       nextLevel = gg.gameOverPlay();
+	       break;
         }
-
     }
 
     // Quit SDL
